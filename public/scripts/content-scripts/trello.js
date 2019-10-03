b6e08331feeba8b4e703d1dbbe1f0e3b62e12bfa
a-silent <tykhyi.andrii@gmail.com>
2019-10-03 23:40:05 +0300

    added github and trello

---
+++ public/scripts/content-scripts/trello.js
@@ -0,0 +1,49 @@
+'use strict';
+/* global createTag */
+try {
+    // const getProject = () => {
+    //     const project = searchElem('.board-header-btn-text');
+    //     return project ? project.textContent.trim() : '';
+    // };
+
+    // wobblyButton.renderButton('.window-header:not(.wobbly)', elem => {
+    //     const rootElement = searchElem(elem);
+    //     const actionButton =
+    //         searchElem('.js-move-card') ||
+    //         searchElem('.js-copy-card') ||
+    //         searchElem('.js-archive-card') ||
+    //         searchElem('.js-more-menu');
+    //     if (!actionButton || !rootElement || rootElement.classList.contains('wobbly')) {
+    //         return;
+    //     }
+
+    //     const getDescription = () => {
+    //         const description = searchElem('.window-title h2', rootElement);
+    //         return description ? description.textContent.trim() : '';
+    //     };
+
+    //     rootElement.classList.add('wobbly');
+    //     const container = createTag('div', 'button-link trello-tb-wrapper');
+    //     let link = createTag('a', 'wobbly', 'Start timer');
+    //     link.style = `
+    //         cursor: pointer;
+    //         padding-left: 20px;
+    //         background: url(${chrome.extension.getURL('images/favicon.svg')}) no-repeat;
+    //         background-size: contain;
+    //     `;
+    //     link.onclick = () =>
+    //         wobblyButton.currentTimer ? wobblyButton.timerStop() : wobblyButton.timerStart(true);
+
+    //     wobblyButton.project = getProject();
+    //     wobblyButton.task = getDescription();
+    //     wobblyButton.issue = encodeURI(wobblyButton.task);
+
+    //     link.title = `${wobblyButton.task} - ${wobblyButton.project}`;
+
+    //     container.appendChild(link);
+    //     actionButton.parentNode.insertBefore(container, actionButton);
+    //     wobblyButton.link = link;
+    // });
+} catch (error) {
+    console.log('error', error);
+}
--- src/originHosts.js
+++ src/originHosts.js
@@ -2,16 +2,26 @@ export default {
     'atlassian.com': {
         url: '*://*.atlassian.com/*',
         name: 'Atlassian / Jira',
-        file: 'jira.js'
+        file: 'jira.js',
     },
     'atlassian.net': {
         url: '*://*.atlassian.net/*',
         name: 'Atlassian / Jira',
-        file: 'jira.js'
+        file: 'jira.js',
     },
     'jira.com': {
         url: '*://*.atlassian.net/*',
         name: 'Jira',
-        file: 'jira.js'
-    }
-}
+        file: 'jira.js',
+    },
+    'github.com': {
+        url: '*://github.com/*',
+        name: 'Github',
+        file: 'github.js',
+    },
+    'trello.com': {
+        url: '*://*.trello.com/*',
+        name: 'Trello',
+        file: 'trello.js',
+    },
+};
