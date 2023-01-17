4cf1f2fc0c69a539679a29f85bb969363fab21f4
Gena3272 <genabirchenko@icloud.com>
2023-01-17 14:44:06 +0200

    WOB-2728 Сreating integration with GitLab

--- public/scripts/content-scripts/gitlab.js
+++ public/scripts/content-scripts/gitlab.js
@@ -2,58 +2,86 @@ function insertAfter(el, referenceNode) {
     referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
 }
 
-wobblyButton.renderButton('.issue-details .detail-page-description:not(.wobbly)', function(elem){
-    let rootElement = searchElem(elem)
-    if(!rootElement) return
+wobblyButton.renderButton('.issue-details .detail-page-description', function(elem){
+    const rootElement = searchElem(elem);
+    if (!rootElement) return;
 
-    wobblyButton.project = searchElem('.breadcrumbs-list li:nth-last-child(3) .breadcrumb-item-text').textContent
-    wobblyButton.task = searchElem('.breadcrumbs-list li:last-child .breadcrumbs-sub-title').textContent
-    wobblyButton.detail = searchElem('.title', rootElement).textContent
-    wobblyButton.issue = `${wobblyButton.task} ${wobblyButton.detail}`
+    const getNumTaskText = () => {
+        const numTask = searchElem('.breadcrumbs-links li:last-child a');
+        return numTask ? numTask.textContent.split(' ').pop() : '';
+    }
 
-    rootElement.classList.add('wobbly')
-    let container = searchElem('.detail-page-header-body')
+    const getDetailTask = () => {
+        const detailTask = searchElem('.title', rootElement);
+        return detailTask ? detailTask.textContent : '';
+    }
+
+    wobblyButton.detail = getDetailTask();
+    wobblyButton.task = getNumTaskText();
+    wobblyButton.issue = `${wobblyButton.task} ${wobblyButton.detail}`;
+
+    let container = searchElem('.detail-page-header-body');
     let link = createTag('a', 'wobbly', 'Start timer')
     link.style.cursor = 'pointer'
     link.style = `
-        cursor: pointer;
-        padding-left: 20px;
-        background: url(${chrome.extension.getURL("images/favicon.svg")}) no-repeat;
-        background-position: -13px 50%;
-        background-size: 50% 50%;
+    cursor: pointer;
+    padding-left: 20px;
+    background: url(${chrome.extension.getURL("images/favicon.svg")}) no-repeat;
+    background-position: -13px 50%;
+    background-size: 50% 50%;
     `
-    link.onclick = () => wobblyButton.currentTimer ?  wobblyButton.timerStop() : wobblyButton.timerStart()
-    link.title = `${wobblyButton.task} / ${wobblyButton.detail} - ${wobblyButton.project}`
+    insertAfter(link,container);
+    container.appendChild(link);
 
+    link.onclick = () => {
+        wobblyButton.currentTimer ? wobblyButton.timerStop() : wobblyButton.timerStart(`${getNumTaskText()} ${getDetailTask()}`);
+    }
 
-    insertAfter(link,container)
-    wobblyButton.link = link
+    link.title = `${getNumTaskText()} ${getDetailTask()}`;
+
+    wobblyButton.link = link;
+    if(link) {
+        const firstLink = container.getElementsByClassName('wobbly');
+        console.log(firstLink[0], '123');
+        firstLink[0].style = `display: none;`;
+    }
 })
 
-wobblyButton.renderButton('.merge-request-details .detail-page-description:not(.wobbly)', function(elem){
+wobblyButton.renderButton('.merge-request-details .detail-page-description', function(elem){
     let rootElement = searchElem(elem)
     if(!rootElement) return
 
-    wobblyButton.project = searchElem('.breadcrumbs-list li:nth-last-child(3) .breadcrumb-item-text').textContent
-    wobblyButton.task = searchElem('.breadcrumbs-list li:last-child .breadcrumbs-sub-title').textContent
-    wobblyButton.detail = searchElem('.title', rootElement).textContent
-    wobblyButton.issue = `${wobblyButton.task} ${wobblyButton.detail}`
+    const getNumTaskText = () => {
+        const numTask = searchElem('.breadcrumbs-links li:last-child a');
+        return numTask ? numTask.textContent.split(' ').pop() : '';
+    }
 
-    rootElement.classList.add('wobbly')
-    let container = searchElem('.detail-page-header-body')
+    const getDetailTask = () => {
+        const detailTask = searchElem('h1.title');
+        return detailTask ? detailTask.textContent : '';
+    }
+
+    wobblyButton.detail = getDetailTask();
+    wobblyButton.task = getNumTaskText();
+    wobblyButton.issue = `${wobblyButton.task} ${wobblyButton.detail}`;
+
+    let container = searchElem('.detail-page-header-body');
     let link = createTag('a', 'wobbly', 'Start timer')
     link.style.cursor = 'pointer'
     link.style = `
-        cursor: pointer;
-        padding-left: 20px;
-        background: url(${chrome.extension.getURL("images/favicon.svg")}) no-repeat;
-        background-position: -13px 50%;
-        background-size: 50% 50%;
+    cursor: pointer;
+    padding-left: 20px;
+    background: url(${chrome.extension.getURL("images/favicon.svg")}) no-repeat;
+    background-position: -13px 50%;
+    background-size: 50% 50%;
     `
-    link.onclick = () => wobblyButton.currentTimer ?  wobblyButton.timerStop() : wobblyButton.timerStart()
-    link.title = `${wobblyButton.task} / ${wobblyButton.detail} - ${wobblyButton.project}`
+    container.appendChild(link);
 
+    link.onclick = () => {
+        wobblyButton.currentTimer ? wobblyButton.timerStop() : wobblyButton.timerStart(`${getNumTaskText()} ${getDetailTask()}`);
+    }
 
-    insertAfter(link,container)
-    wobblyButton.link = link
+    link.title = `${getNumTaskText()} ${getDetailTask()}`;
+
+    wobblyButton.link = link;
 })
